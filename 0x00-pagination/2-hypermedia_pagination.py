784d6bb32e00efe127e892ade04e378ce2e832d7
Zkrallah <muhammad.heshamyt@gmail.com>
2024-07-23 01:29:37 +0300

    Return some meta-data about the page.

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -44,19 +44,13 @@ class Server:
     
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """ Get hypermedia pagination details. """
-        data = self.get_page(page, page_size)
-        dataset = self.dataset()
-        total_pages = math.ceil(len(dataset) / page_size)
-        
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
-
+        total_pages = math.ceil(len(self.dataset()) / page_size)
         return {
-            'page_size': len(data),
-            'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages,
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page + 1 <= total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
         }
     
