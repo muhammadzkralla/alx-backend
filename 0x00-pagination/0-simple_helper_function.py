5bdea7ef1e42ddf7c9c6ba393b4aacd2e0facd61
Zkrallah <muhammad.heshamyt@gmail.com>
2024-07-23 01:00:54 +0300

    Index range function implemented.

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ Index range.
 
 from typing import Tuple
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Return a tuple of size two containing a start index and an end index. """
     start_index = (page - 1) * page_size
     end_index = page * page_size
