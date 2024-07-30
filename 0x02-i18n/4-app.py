e19e21f04f44812883d45421271de8f411f5910b
Zkrallah <muhammad.heshamyt@gmail.com>
2024-07-31 00:29:42 +0300

    Pushing without testing the babel.cfg configuration files.

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -4,28 +4,31 @@
 4. Hello world Flask app with template and langs.
 """
 
+import re
+import babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
+
+
+class Config():
+    """ Configuration for babel translation """
+    LANGUAGES = ["en", "fr"]
+
 
 app = Flask(__name__)
-
-
-class Config:
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
+app.config.from_object(Config())
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
     """ get_locale function implementation. """
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
-        return locale
+    lang = request.args.get('locale')
+    if lang is not None:
+        if lang in app.config['LANGUAGES']:
+            return lang
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
@@ -36,4 +39,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000")
